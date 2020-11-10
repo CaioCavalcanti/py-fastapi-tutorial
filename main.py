943db3b12c06166833c071469878b15753b2295c
Caio Cavalcanti <caiofabiomc@gmail.com>
2020-11-10 23:17:51 +0100

    path parameters

--- main.py
+++ main.py
@@ -1,8 +1,48 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    """
+    Get data about the current user.
+
+    Order matters: because path operations are evaluated in order, you
+    need to make sure that the path for /users/me is declared before the
+    one for /users/{user_id}:
+    """
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}

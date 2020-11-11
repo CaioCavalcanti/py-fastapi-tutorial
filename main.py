94a1af8846bc4751758a8da0c023a1899714ce48
Caio Cavalcanti <caiofabiomc@gmail.com>
2020-11-11 22:06:16 +0100

    query parameters

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import uvicorn
-
 from fastapi import FastAPI
 from enum import Enum
+from typing import Optional
 
 app = FastAPI()
 
@@ -12,14 +11,37 @@ class ModelName(str, Enum):
     lenet = "lenet"
 
 
+fake_items_db = [{"item_name": "Foo"}, {
+    "item_name": "Bar"}, {"item_name": "Baz"}]
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: str, needy: str, q: Optional[str] = None, short: bool = False):
+    """
+    Get an item with the given item_id.
+
+    The Optional in Optional[str] is not used by FastAPI (FastAPI will
+    only use the str part), but the Optional[str] will let your editor
+    help you finding errors in your code.
+    """
+    item = {"item_id": item_id, "needy": needy}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"})
+
+    return item
 
 
 @app.get("/users/me")
@@ -39,6 +61,18 @@ async def read_user(user_id: str):
     return {"user_id": user_id}
 
 
+@app.get("/users/{user_id}/items/{items_id}")
+async def read_user_items(user_id: str, item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name == ModelName.alexnet:
@@ -48,7 +82,3 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
